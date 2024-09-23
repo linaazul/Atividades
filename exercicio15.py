de5fa3ecc8b1db6fc6d559c32f7e2c6d04553094
Ana Guimarães <ana.carolina.gui.ramos@gmail.com>
2024-09-23 17:29:29 -0300

    Exercicio sobre closure

---
+++ exercicio15.py
@@ -0,0 +1,11 @@
+# Função que cria prefixos para strings
+# Crie uma função que recebe um prefixo como argumento e retorna uma função que recebe uma string e retorna essa string com o prefixo anexado.
+
+def cria_prefixo(prefixo):
+    def retornar_prefixo_anexado(nome):
+        return f'{prefixo} {nome}'
+    return retornar_prefixo_anexado
+
+
+prefixa_com_mr = cria_prefixo('Miss')
+print(prefixa_com_mr('dumbdogs'))
